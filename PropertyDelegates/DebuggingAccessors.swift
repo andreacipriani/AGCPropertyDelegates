64c35c09ab0febf0d621431b8170dd541e159cf4
Andrea Cipriani <andrea.cipriani@soundcloud.com>
2019-06-10 21:57:36 -0700

    Cut thread description and change print format

--- PropertyDelegates/DebuggingAccessors.swift
+++ PropertyDelegates/DebuggingAccessors.swift
@@ -2,27 +2,32 @@ import Foundation
 
 @propertyDelegate
 public final class DebuggingAccessors<T> {
-
     private let label: String
     private var _value: T?
 
-    public init(_ label: String) {
+    public init(_ label: String, shouldPrintCaller: Bool = true) {
         self.label = label
     }
     
     public var value: T? {
         get {
-            print("Accessed property * \(label) * with value \(makeDescription(of:_value)) on thread \(currentThreadDescription)")
+            print("get \(label) = \(makeDescription(of: _value)) on \(currentThreadDescription)")
             return _value
         } set {
-            print("Property * \(label) * set from \(makeDescription(of: value)) to \(makeDescription(of: newValue)) on thread \(currentThreadDescription)")
+            print("set \(label) = \(makeDescription(of: value)) on \(currentThreadDescription)")
             _value = newValue
         }
     }
-    
+
     private var currentThreadDescription: String {
-        return Thread.current.description
+        let fullDescription = Thread.current.description
+        if let range = fullDescription.range(of: "number = ") {
+            let threadNumber = fullDescription[range.upperBound...].prefix(1)
+            return String("thread: \(threadNumber)")
+        }
+        return fullDescription
     }
+    
     private func makeDescription(of optionalValue: T?) -> String {
         return optionalValue != nil ? "\(optionalValue!)" : "nil"
     }
